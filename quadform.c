313bf5ff96ccb3d90287786096a9e7c994f91645
JJ Shep <thefile.market@gmail.com>
2023-03-07 23:55:49 -0500

    Make program print solution once if only one real solution

--- quadform.c
+++ quadform.c
@@ -3,6 +3,7 @@
 
 int main() {
   int imaginary = 0;
+  int onereal = 0;
   double term1, term2, term3;
   printf("Enter values of A, B, and C, separated by spaces: ");
   scanf("%lf %lf %lf",&term1,&term2,&term3);
@@ -11,6 +12,7 @@ int main() {
     double temp = term1;
     term1 = term2 * -1;
     term2 = term2 * term2 - 4 * temp * term3;
+    onereal -= term2;
     if (term2 < 0) {
       imaginary = 1;
       term2 *= -1;
@@ -20,6 +22,10 @@ int main() {
 
   term2 = sqrt(term2);
   if (!imaginary) {
+    if (onereal == 0) {
+      printf("x = %.4lf\n",(term1+term2)/term3);
+      return 0;
+    }
     printf("x = %.4lf\nx = %.4lf\n",(term1+term2)/term3,(term1-term2)/term3);
     return 0;
   }
